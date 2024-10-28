78851054586939b504703374331da7d8dd32db7d
kaiizzz <billzhukl@gmail.com>
2024-10-28 21:49:51 +1100

    init

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+// Define a route for the home page
+app.get('/', (req, res) => {
+  res.send('Hello, welcome to my website!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
